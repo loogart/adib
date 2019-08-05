e7b66e939b14ffab85840e97419e8fba938df7cd
Chris Soueidan <chris@loogart.com>
2019-08-05 10:07:48 -0400

    nouveau code par Fred Fauvel

--- js/shows.js
+++ js/shows.js
@@ -5,9 +5,43 @@ $.ajax({
     jsonpCallback: 'processJSONPResponse',
     contentType: "application/json; charset=utf-8",
     success: function (result, status, xhr) {
-        console.log(result);
-        // créer le html
+        var app = document.getElementById('root') 
+
+        var container = document.createElement('div')
+        container.setAttribute('class', 'div')
+        app.appendChild(container)
+
+        for(var i = 0; i < result.spectacles.length; i++) {
+            var spectacle = result.spectacles[i];
+            var show = document.createElement('div');
+            show.setAttribute('class', 'spectacle')
+               
+            var titre = document.createElement('h4');      
+            titre.setAttribute('class', 'info')   
+            titre.textContent = spectacle.name
+       
+            var date = document.createElement('h5')
+            date.setAttribute('class', 'date')
+            date.textContent = spectacle.full_date
+       
+            var location = document.createElement('h5')
+            location.setAttribute('class', 'info')
+            location.textContent = spectacle.ville    
+       
+       
+            var tickets = document.createElement('a')
+            tickets.textContent = "Billets"
+            tickets.setAttribute('class', 'btn btn-lg btn-custom-2')
+            tickets.setAttribute('href', spectacle.url)
+            tickets.setAttribute('target', '_blank')
         
+
+            container.appendChild(show)
+            show.appendChild(titre)
+            show.appendChild(date)
+            show.appendChild(location)
+            show.appendChild(tickets)
+        }
     },
     error: function (xhr, status, error) {
         console.log("Result: " + status + " " + error + " " + xhr.status + " " + xhr.statusText)
